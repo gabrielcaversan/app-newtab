4db2e085888ef3380e6206600de0d60fc812bcc8
gabrielcaversan <gabriel.felipe.caversan@gmail.com>
2022-10-07 00:35:24 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom"
+import React from "react"
+import Lista from './componentes/Lista'
+
+const rootComponent = document.getElementById("root")
+ReactDOM.render(
+<Lista />
+, rootComponent)
